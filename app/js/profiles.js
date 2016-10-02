1adfe7ac97f69148edf3c6d96b1be152d846ea22
BigD <dlau373@gmail.com>
2016-10-02 00:59:42 -0700

    Incorporated jquery-validate plugin into cpt entry input. Need to finish addCPT function

--- app/js/profiles.js
+++ app/js/profiles.js
@@ -1,11 +1,32 @@
 // Handler for .ready() call
 $(function(){
   init()
-  $('#inputAdd').on('click',addCpt)
+
+  getAllCpt();
+  // Set event listeners on buttons
+  //$('#inputAdd').on('click',addCpt);
   $('select[name="cptList"]').select2({
-     color:'black'
+     placeholder:'Select a code'
   });
+
+  $('#cptAdd').validate({
+    rules:{
+      addCptInput:{
+        minlength:5,
+        digits:true
+      }
+    },
+    messages:{
+      addCptInput:{
+        minlength: 'CPT Code must be length of 5'
+      }
+    },
+    errorLabelContainer:'#errorMsg',
+    submitHandler:addCpt
+  })
+
 });
+
 
 // Helper functions
 // Initialize store.js
@@ -15,9 +36,66 @@ function init() {
         return
     }}
 
+// CPT JSON Data Model
+var cptObj={
+  id:'',
+  text:'',
+  place:'',
+  days:'',
+  emg:'',
+  modA:'',
+  modB:'',
+  modC:'',
+  modD:'',
+  epsdt:'',
+}
+
+// Methods to add, remove, save CPT codes
+// to localStorage with store.js
+
+// Get array of cpt objects
+function getCptArray(){
+  var allObjs=store.getAll()
+  delete allObjs.cptList;
+  var array=$.map(allObjs,function(value,index){
+  return value;
+})
+  return array
+}
+
+// StorageEvent is not supported on same page
+// Must use function to update select list
+function updateSelect(){
+  $('select[name="cptList"]').select2({
+     placeholder:'Select a code',
+     data:getCptArray()
+   })
+}
+
+// Initialize and return obj of all CPT Codes
+function getAllCpt(){
+  if (store.get('cptList')==null){
+    store.set('cptList',[]);
+  }
+  updateSelect()
+  }
+
 function addCpt(){
-  console.log('adding CPT')
   var cptCode=$('#addCptInput').val()
-  var selectBox=$('select[name="cptList"]')
-  selectBox.prepend("<option>"+cptCode+"</option>")
+  var $selectBox=$('select[name="cptList"]').select2();
+  var tempObj= $.extend({},cptObj)
+  tempObj.id=cptCode;
+  tempObj.text=cptCode;
+  store.set('cpt'+cptCode,tempObj);
+  updateSelect()
+  $('#addCptInput').val('')
+  $selectBox.val(cptCode).trigger('change');
+}
+
+function removeCpt(){
+
+}
+
+function saveCPT(){
+
 }
